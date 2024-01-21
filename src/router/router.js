7306d7746ccb5c2e9508472077996dc073453eea
cheesemitch <edin.cizmic@agnoscis.io>
2024-01-22 00:14:26 +0100

    changed router to webHashHistory to fix the 404 error on refresh and added a 404.html

--- src/router/router.js
+++ src/router/router.js
@@ -1,5 +1,5 @@
 // router/index.js
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import Home from '../views/Home.vue';
 import Contact from '../views/Contact.vue';
 import Imprint from '../views/Imprint.vue';
@@ -32,7 +32,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(base),
+  history: createWebHashHistory(base),
   routes,
 });
 
